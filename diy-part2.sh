a457540904d402f9b7343c944eb058b85c25af76
sekzhao <sekzhao@qq.com>
2024-10-15 08:12:10 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -27,6 +27,13 @@ mv OpenWrt-mihomo/mihomo package/
 git clone https://github.com/hudra0/qosmate.git package/qosmate
 git clone https://github.com/hudra0/luci-app-qosmate.git package/luci-app-qosmate
 
+#添加Mosdns
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
+rm -rf feeds/packages/net/v2ray-geodata
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+
 #修改nginx默认http
 mkdir -p files/etc/config
 
